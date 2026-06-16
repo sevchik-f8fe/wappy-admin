8529140c86bea1703e30083374a6eebbac520ce1
sevchik-f8fe <selddevelopment1111@gmail.com>
2026-06-16 23:00:01 +0300

    dep

--- src/util/axiosConfig.js
+++ src/util/axiosConfig.js
@@ -23,7 +23,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://wappy.online',
     withCredentials: true,
     timeout: 5000,
 });
